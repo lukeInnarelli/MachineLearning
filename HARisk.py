c1f73dffe0d9e312adf5bfe0d19765ccb4beb148
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2024-09-04 18:33:20 -0700

    Address PEP8 style guidelines

--- HARisk.py
+++ HARisk.py
@@ -6,40 +6,39 @@ from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def learnRisk():
-    #gets the dataset
+    # gets the dataset
     dataset2 = open('archive/heart.csv', 'r')
     o = csv.reader(dataset2)
     dataset3 = []
     for dataset2 in o:
         dataset3.append(dataset2)
     dataset4 = np.array(dataset3[1:])
-    dataset5 = np.zeros((len(dataset4),len(dataset4[1])))
-    for i in range(0,len(dataset4)):
-       for j in range(0,len(dataset4[i])):
-            dataset5[i,j] = float(dataset4[i,j])
-    #orders the dataset
-    datasetInput = dataset5[:,:13]
-    datasetTarget = dataset5[:,13]
+    dataset5 = np.zeros((len(dataset4), len(dataset4[1])))
+    for i in range(0, len(dataset4)):
+        for j in range(len(dataset4[i])):
+            dataset5[i, j] = float(dataset4[i, j])
+    # orders the dataset
+    datasetInput = dataset5[:, :13]
+    datasetTarget = dataset5[:, 13]
     inputs_train, inputs_test, targets_train, targets_test = preprocess(datasetInput, datasetTarget)
 
-    #puts the dataset into the classifier to train
+    # puts the dataset into the classifier to train
     classifier = MLPClassifier()
     classifier.fit(inputs_train, targets_train)
     results = classifier.predict(inputs_test)
 
-    #prints out the results
+    # prints out the results
     print("predicted values")
     print(results)
     print("actual values")
     print(targets_test)
 
-    #one line accuracy of the machine learning
+    # one line accuracy of the machine learning
     print(np.mean(np.equal(results, targets_test)))
-    #display_confusion_matrix(targets_test,results)
+    # display_confusion_matrix(targets_test,results)
 
-#preprocessing and making a random test group to pull from
+# preprocessing and making a random test group to pull from
 def preprocess(inputs, targets):
     inputs_train, inputs_test, targets_train, targets_test = train_test_split(
         inputs, targets, test_size=0.10, random_state=0,
@@ -48,7 +47,6 @@ def preprocess(inputs, targets):
     inputs_test = inputs_test / np.float32(255.)
     return inputs_train, inputs_test, targets_train, targets_test
 
-#
 def display_confusion_matrix(target, predictions, labels=['Low Risk', 'High Risk'], plot_title='Performance'):
     cm = confusion_matrix(target, predictions)
     cm_display = ConfusionMatrixDisplay(cm, display_labels=labels)
