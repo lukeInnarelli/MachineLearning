1ff1be24fb910c80fc4730e398456f7f75ae1b6a
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2024-09-11 19:25:57 -0700

    Refactor plotting code

--- HARisk.py
+++ HARisk.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
+
+PLOT_FOLDER = Path('plots')
 
 def get_data():
     dataset = np.genfromtxt(
@@ -11,29 +15,32 @@ def get_data():
     return preprocess(dataset[:, :13], dataset[:, 13])
 
 def learnRisk():
-
     inputs_train, inputs_test, targets_train, targets_test = get_data()
 
     # puts the dataset into the classifier to train
-    i = 0
     tests = np.array([470, 475, 480, 485, 490, 495, 1000])
     classes = np.zeros(len(tests))
-    for hidden_layer_sizes in tests:
+    for i, hidden_layer_sizes in enumerate(tests):
         classifier = MLPClassifier(random_state=0, max_iter=1200, hidden_layer_sizes=hidden_layer_sizes, learning_rate_init=0.06)
         classifier.fit(inputs_train, targets_train)
-        results = classifier.predict(inputs_test)
+        predictions_train = classifier.predict(inputs_train)
+        predictions_test = classifier.predict(inputs_test)
 
         # one line accuracy of the machine learning
-        print(np.mean(np.equal(results, targets_test)))
+        print(f'\nTrain Accuracy for {hidden_layer_sizes = }: {np.mean(np.equal(predictions_train, targets_train)) * 100:.3f}%')
+        print(f'Test Accuracy for {hidden_layer_sizes = }: {np.mean(np.equal(predictions_test, targets_test)) * 100:.3f}%')
         plt.title('Hyperparameter experimentation')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
         plt.plot(range(len(classifier.loss_curve_)), classifier.loss_curve_, label=f'{hidden_layer_sizes=}')
         classes[i] = classifier.best_loss_
-        i = i + 1
-        # display_confusion_matrix(targets_test, results, plot_title='Test Performance')
+        display_confusion_matrix(targets_train, predictions_train, plot_title=f'Train Performance {hidden_layer_sizes}')
+        display_confusion_matrix(targets_test, predictions_test, plot_title=f'Test Performance {hidden_layer_sizes}')
     plt.legend()
-    plt.show()
+    PLOT_FOLDER.mkdir(exist_ok=True)
+
+    plt.savefig(PLOT_FOLDER / 'epoch_vs_loss.png')
+    plt.close()
     plt.title('Final Loss')
     plt.xlabel('Experiment number')
     plt.ylabel('Loss')
@@ -41,8 +48,8 @@ def learnRisk():
     for x in range(len(tests)):
         ext[x] = x + 1
     plt.plot(ext, classes)
-    plt.legend()
-    plt.show()
+    plt.savefig(PLOT_FOLDER / 'hidden_layer_sizes_vs_best_loss.png')
+    plt.close()
 
 # preprocessing and making a random test group to pull from
 def preprocess(inputs, targets):
@@ -59,7 +66,8 @@ def display_confusion_matrix(target, predictions, labels=['Low Risk', 'High Risk
     fig, ax = plt.subplots()
     cm_display.plot(ax=ax)
     ax.set_title(plot_title)
-    plt.show()
+    plt.savefig(PLOT_FOLDER / f'{plot_title.lower().replace(" ", "_")}.png')
+    plt.close()
 
 if __name__ == '__main__':
     learnRisk()
