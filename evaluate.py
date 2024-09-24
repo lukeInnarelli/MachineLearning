ce25922bc23102d0d0aae6522658df5b645c8303
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2024-09-23 19:05:01 -0700

    Refactor feature evaluation into separate file

---
+++ evaluate.py
@@ -0,0 +1,34 @@
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.inspection import permutation_importance
+
+from experiment import get_data
+
+def main(argname):
+    with open('archive/heart.csv', mode='r') as data_file:
+        colnames = next(data_file).strip().split(',')
+    with open(f'best_model_{argname}.pkl', mode='rb') as model_file:
+        model = pickle.load(model_file)
+
+    inputs_train, inputs_test, targets_train, targets_test = get_data()
+
+    per = permutation_importance(model, inputs_test, targets_test, n_repeats=30, random_state=0)
+    box_array = np.zeros((2, 13))
+    strings = []
+    for i in per.importances_mean.argsort()[::-1]:
+        print(
+            f"{colnames[i]:<8}"
+            f"{per.importances_mean[i]:.3f}"
+            f" +/- {per.importances_std[i]:.3f}"
+        )
+        box_array[0, i] = per.importances_mean[i]
+        box_array[1, i] = per.importances_std[i]
+        strings.append(colnames[i])
+    plt.boxplot(box_array)
+    plt.show()
+
+if __name__ == '__main__':
+    # main('learning_rate_init')
+    main('hidden_layer_sizes')
--- experiment.py
+++ experiment.py
@@ -1,9 +1,9 @@
 from pathlib import Path
+import pickle
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-from sklearn.inspection import permutation_importance
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,14 +16,10 @@ def get_data():
     return preprocess(dataset[:, :13], dataset[:, 13])
 
 def learn_risk(argname, argvals):
-    with open('archive/heart.csv') as data_file:
-        colnames = next(data_file).strip().split(',')
     inputs_train, inputs_test, targets_train, targets_test = get_data()
 
     # puts the dataset into the classifier to train
     models = []
-    per_predictions = []
-    per_models = []
     for i, value in enumerate(argvals):
         classifier = MLPClassifier(random_state=0, max_iter=1200, **{argname: value},)
         classifier.fit(inputs_train, targets_train)
@@ -33,32 +29,19 @@ def learn_risk(argname, argvals):
         display_confusion_matrix(targets_train, predictions_train, plot_title=f'{argname} Train Performance')
         display_confusion_matrix(targets_test, predictions_test, plot_title=f'{argname} Test Performance')
         # one line accuracy of the machine learning
-        # print(f'\nTrain Accuracy for {argname} with {value = }: {np.mean(np.equal(predictions_train, targets_train)) * 100:.3f}%')
-        # print(f'Test Accuracy for {argname} with {value = }: {np.mean(np.equal(predictions_test, targets_test)) * 100:.3f}%')
+        print(f'\nTrain Accuracy for {argname} with {value = }: {np.mean(np.equal(predictions_train, targets_train)) * 100:.3f}%')
+        print(f'Test Accuracy for {argname} with {value = }: {np.mean(np.equal(predictions_test, targets_test)) * 100:.3f}%')
+        print(f'Best Loss for {argname} with {value = }: {classifier.best_loss_}%')
         models.append(classifier)
 
-        per_models.append(classifier)
-        per_predictions.append(predictions_test)
+    plot(argvals, models, argname)
 
-    # plot(argvals, models, argname, targets_train, predictions_train, targets_test, predictions_test)
-
-    # print(inputs_test)
-    print(per_predictions)
-    per = permutation_importance(per_models[1], inputs_test, targets_test, n_repeats=30, random_state=0)
-    box_array = np.zeros((2, 13))
-    strings = []
-    for i in per.importances_mean.argsort()[::-1]:
-        print(
-            f"{colnames[i]:<8}"
-            f"{per.importances_mean[i]:.3f}"
-            f" +/- {per.importances_std[i]:.3f}"
-        )
-        box_array[0, i] = per.importances_mean[i]
-        box_array[1, i] = per.importances_std[i]
-        strings.append(colnames[i])
-    plt.boxplot(box_array)
-    plt.show()
-
+    # Compute argmax to attain best model
+    best_model = min((model.best_loss_, model) for model in models)[1]
+    model_file_name = f'best_model_{argname}.pkl'
+    with open(model_file_name, mode='wb') as model_file:
+        pickle.dump(best_model, model_file)
+    print(f'Successfully saved `{model_file_name}`')
 
 def plot(argvals, models, argname):
     for value, classifier in zip(argvals, models):
@@ -103,5 +86,5 @@ def display_confusion_matrix(target, predictions, labels=['Low Risk', 'High Risk
     plt.close()
 
 if __name__ == '__main__':
-    # learn_risk('learning_rate_init', np.array([380, 400, 470]))
+    learn_risk('learning_rate_init', np.array([380, 400, 470]))
     learn_risk('hidden_layer_sizes', [(100,) * i for i in range(1, 5)])
